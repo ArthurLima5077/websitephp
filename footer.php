65538849f2dfe91458a5b562164953a0aed2d4cb
Arthur <arthurphillipegalvao@gmail.com>
2025-09-27 22:02:14 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,23 @@
+<hr>
+<footer class="page-footer" 
+        style="position: fixed; bottom: 0; left: 0; width: 100%; margin: 0;">
+  <div class="container" style="padding-top:2px; padding-bottom:2px;">
+    <div class="row" style="margin-bottom:0;">
+  </div>
+  <div class="footer-copyright">
+      © 2024 Copyright Text
+      <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+    </div>
+  </div>
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+<style>
+  body {
+    margin: 0;
+    padding-bottom: 50px; 
+  }
+</style>
+</body>
+</html>

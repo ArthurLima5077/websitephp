e71f2feb59ebb386ea3b2d0de9622c409e44520e
Arthur <arthurphillipegalvao@gmail.com>
2025-09-27 22:02:37 -0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php require_once __DIR__ . '/helpers.php'; ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>  
+ <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+  <header>
+    <h1></h1>
+    <nav>
+      <a href="index.php">Home</a> |
+      <a href="sobre.php">Sobre a Oficina</a> |
+      <a href="galeria.php">Galeria</a> |
+      <a href="contato.php">Contato</a>
+    </nav>
+    <hr>
+  </header>
